1ffa0ec00e3378c84466bd3573bed33eaac4dcfe
jns-br <jonasdone@gmail.com>
2020-02-05 17:39:59 +0100

    update env config

--- server/config/dev.env.js
+++ server/config/dev.env.js
@@ -1,6 +1,4 @@
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
-})
+module.exports = {
+  NODE_ENV: '"development"',
+  DB_URL: 'mongodb://localhost:27017/Mini-IPIP'
+};

db0b378e1216fafddbdf59a06fa3a57bd2c6dfce
jns-br <jonasdone@gmail.com>
2020-02-05 17:42:04 +0100

    update webpack config

--- server/build/webpack.base.conf.js
+++ server/build/webpack.base.conf.js
@@ -12,11 +12,9 @@ module.exports = {
     app: './src/main.js'
   },
   output: {
-    path: config.build.assetsRoot,
+    path: config.dev.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    publicPath: config.dev.assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],

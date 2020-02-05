70cdd8ba2604c519ef3b4a8a7edb0e1b5d21a59c
jns-br <jonasdone@gmail.com>
2020-02-05 17:53:45 +0100

    refactor utils

--- server/build/utils.js
+++ server/build/utils.js
@@ -1,28 +1,26 @@
-var path = require('path')
-var config = require('../config')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path');
+const config = require('../config');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 exports.assetsPath = function (_path) {
-  var assetsSubDirectory = process.env.NODE_ENV === 'production'
-    ? config.build.assetsSubDirectory
-    : config.dev.assetsSubDirectory
+  const assetsSubDirectory = config.dev.assetsSubDirectory;
   return path.posix.join(assetsSubDirectory, _path)
-}
+};
 
 exports.cssLoaders = function (options) {
-  options = options || {}
+  options = options || {};
 
-  var cssLoader = {
+  let cssLoader = {
     loader: 'css-loader',
     options: {
       minimize: process.env.NODE_ENV === 'production',
       sourceMap: options.sourceMap
     }
-  }
+  };
 
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
-    var loaders = [cssLoader]
+    let loaders = [cssLoader];
     if (loader) {
       loaders.push({
         loader: loader + '-loader',
@@ -54,14 +52,14 @@ exports.cssLoaders = function (options) {
     stylus: generateLoaders('stylus'),
     styl: generateLoaders('stylus')
   }
-}
+};
 
 // Generate loaders for standalone style files (outside of .vue)
 exports.styleLoaders = function (options) {
-  var output = []
-  var loaders = exports.cssLoaders(options)
-  for (var extension in loaders) {
-    var loader = loaders[extension]
+  let output = [];
+  let loaders = exports.cssLoaders(options);
+  for (let extension in loaders) {
+    let loader = loaders[extension];
     output.push({
       test: new RegExp('\\.' + extension + '$'),
       use: loader
